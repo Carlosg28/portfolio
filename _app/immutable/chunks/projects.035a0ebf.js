4962a222fa03ba034931b5d81fb04b0833429af6
Carlosg28 <71712358+Carlosg28@users.noreply.github.com>
2025-02-22 21:09:13 +0000

    Deploying to gh-pages from @ Carlosg28/portfolio@384e27d1c2f3fd8259f1e10cc1a62daa984e2f1b 🚀

--- _app/immutable/chunks/projects.035a0ebf.js
+++
@@ -1,1 +0,0 @@
-import"./UIcon.e34bc0a8.js";import"./skills.4c9e5328.js";const s=[],i="Projects";export{s as i,i as t};
--- _app/immutable/chunks/skills.26fc09c2.js
+++ _app/immutable/chunks/skills.26fc09c2.js
@@ -1,4 +1,4 @@
-import{A as n}from"./UIcon.e34bc0a8.js";const m=`# Svelte
+import{A as n}from"./UIcon.e31c121e.js";const m=`# Svelte
 
 ---
 
