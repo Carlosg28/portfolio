a9c240f403ec8374c704ed022689c73dea1a2d6f
Carlos Garcia <cygn333@gmail.com>
2025-03-06 17:11:05 -0500

    replaced /n with <br>

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -23,7 +23,7 @@ export const items: Array<Project> = [
 		slug: 'slick-portfolio-svelte',
 		color: '#ff3e00',
 		description:
-			"Track Your Spending: Easily add, update, and remove expense records.\nOrganized Overview: View all your expenses in a clear, simple list.\nUser-Friendly Interface: Enjoy a straightforward, real-time experience managing your budget.\nReliable Storage: Keep your financial data organized and accessible.",
+			'Track Your Spending: Easily add, update, and remove expense records. <br /> Organized Overview: View all your expenses in a clear, simple list.\nUser-Friendly Interface: Enjoy a straightforward, real-time experience managing your budget.\nReliable Storage: Keep your financial data organized and accessible.',
 		shortDescription:
 			'This project is a backend application for managing expenses using CRUD (Create, Read, Update, Delete) operations. It’s built with FastAPI and MongoDB, and it provides a RESTful API for tracking and managing your expense data.',
 		links: [{ to: 'https://github.com/Carlosg28/expenses-app', label: 'GitHub' }],
